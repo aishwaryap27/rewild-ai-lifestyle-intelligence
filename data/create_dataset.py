7639d3b57ab9bb53d7b007e6d7dd8e3e0173472b
aishurani54 <aishurani54@gmail.com>
2026-03-10 19:12:02 +0530

    Update create_dataset.py

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import random
 random.seed(42)
@@ -21,20 +20,4 @@ captions = [
     "less notifications more presence",
     "running at 6am beats social media",
 ]
-=======
-import pandas as pd
-import random
-random.seed(42)
-categories=["dopamine_detox","minimalism","fitness","slow_living","burnout_recovery"]
-captions=["deleted all my apps,feeling free",
-    "minimalist morning routine no phone",
-    "30 day dopamine detox challenge day 1",
-    "slow morning coffee no screens",
-    "gym over doom scrolling every time",
-    "bought nothing this week, proud of it",
-    "analog journaling changed my life",
-    "screen free sunday is my religion",
-    "less notifications more presence",
-    "running at 6am beats social media",
-]
->>>>>>> ab05f458cb2773e12fe1206b749c2d51ffb9d2f8
+print("Hello World!!")
