7d0552554d0eee49acc555b92608ad285aee8a55
aishwaryap27 <aishwaryapramod23cs@rnsit.ac.in>
2026-03-10 18:55:00 +0530

    updated dataset

---
+++ data/create_dataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import random
+random.seed(42)
+categories = [
+    "dopamine_detox",
+    "minimalism",
+    "fitness",
+    "slow_living",
+    "burnout_recovery"
+]
+captions = [
+    "deleted all my apps, feeling free",
+    "minimalist morning routine no phone",
+    "30 day dopamine detox challenge day 1",
+    "slow morning coffee no screens",
+    "gym over doom scrolling every time",
+    "bought nothing this week, proud of it",
+    "analog journaling changed my life",
+    "screen free sunday is my religion",
+    "less notifications more presence",
+    "running at 6am beats social media",
+]
