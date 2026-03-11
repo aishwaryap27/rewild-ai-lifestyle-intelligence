487dc8bc6ad47812e113b0a18e0d2b244f23ca49
aishwaryap27 <aishwaryapramod23cs@rnsit.ac.in>
2026-03-11 18:30:27 +0530

    updated

---
+++ decision_tree.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+import matplotlib.pyplot as plt'
+import seaborn as sns
+from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.model_selection import train_test_split
+
